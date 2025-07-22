9d1886380d7330d64c923d7f347651914ec2141b
supakron6812022611359 <s6812022611359@email.kmutnb.ac.th>
2025-07-22 16:53:14 +0700

    Update Coding_w04-05.1.c

--- Coding_w04-05.1.c
+++ Coding_w04-05.1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 void countCall() {
-    static int counter = 0;
-    counter++;
-    printf("Call function: counter = %d\n", counter);
+    static int counter = 0;                                      // ประกาศตัวแปร static ทำให้ค่าของตัวแปรนี้คงอยู่ระหว่างการเรียกฟังก์ชัน
+    counter++;                                                   // เพิ่มค่าตัวแปร counter ทีละ 1 ทุกครั้งที่เรียกฟังก์ชัน
+    printf("Call function: counter = %d\n", counter);            // แสดงค่าของ counter
 }
 
 int main() {
-    printf("Starting function calls...\n");
-    countCall();
+    printf("Starting function calls...\n");                       
+    countCall();                                                  // เรียกฟังก์ชัน countCall() ครั้งที่ 1
     printf("After first call:\n");
-    countCall();
+    countCall();                                                  // เรียกฟังก์ชัน countCall() ครั้งที่ 2
     return 0;
 }
