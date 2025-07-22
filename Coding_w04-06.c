d3b72bfa76e50275c61d2fe7af3ccd93dd364073
supakron6812022611359 <s6812022611359@email.kmutnb.ac.th>
2025-07-22 16:45:53 +0700

    Update Coding_w04-06.c

--- Coding_w04-06.c
+++ Coding_w04-06.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-const int GLOBAL_RATE = 10;
-void calculate() {
-const int LOCAL_BONUS = 50;
+const int GLOBAL_RATE = 10;                   // ประกาศตัวแปรคงที่แบบ global ที่ใช้ได้ทั่วทั้งโปรแกรม
+void calculate() {                            // ฟังก์ชัน calculate ใช้แสดงค่าของ GLOBAL_RATE และ LOCAL_BONUS
+const int LOCAL_BONUS = 50;                    // ตัวแปรคงที่แบบ local ใช้ได้เฉพาะในฟังก์ชันนี้
 
-printf ("GLOBAL_RATE = %d\n", GLOBAL_RATE);
-printf("LOCAL_BONUS = %d\n", LOCAL_BONUS);
+printf ("GLOBAL_RATE = %d\n", GLOBAL_RATE);   // แสดงค่า GLOBAL_RATE ซึ่งเป็นตัวแปร global
+printf("LOCAL_BONUS = %d\n", LOCAL_BONUS);    // แสดงค่า LOCAL_BONUS ซึ่งเป็นตัวแปร local ของฟังก์ชัน
 }
 int main () {
 calculate() ;
-printf ("GLOBAL_RATE = %d\n", GLOBAL_RATE);
+printf ("GLOBAL_RATE = %d\n", GLOBAL_RATE);    // แสดงค่า GLOBAL_RATE อีกครั้งใน main
 
 return 0;
 }
